2810f6930db438b53a2b0cc40dc11537e408b460
tumelogasetlolwe <ttyeyo@gmail.com>
2024-06-12 13:21:06 +0200

    First code up

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    public void Tumelo(){
+
+
+    }
+
+
+
+
+
+public void TeeBabe(){
+
+
+}
+
+
+
+}
